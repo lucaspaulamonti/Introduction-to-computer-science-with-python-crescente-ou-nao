5db9bbb76db91d50bd8b258a3beb39bb85ebfb11
Lucas Monti <108158100+MontiLucas@users.noreply.github.com>
2022-07-22 15:35:05 -0300

    Create crescente-ou-nao.py
    main content

---
+++ crescente-ou-nao.py
@@ -0,0 +1,10 @@
+# Faça um programa em Python que valide se as informações estão em ordem crescente.
+primeiro=int(input("Informe o primeiro número inteiro: "))
+segundo=int(input("Informe o segundo número inteiro: "))
+terceiro=int(input("Informe o terceiro número inteiro: "))
+if (primeiro < segundo) and (segundo < terceiro):
+    print("crescente")
+else:
+    print("não está em ordem crescente")
+# O resultado dos testes com seu programa foi:
+# Parabéns! Todos os testes tiveram sucesso! 
